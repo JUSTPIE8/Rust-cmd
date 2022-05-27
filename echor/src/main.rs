161739498271ed8d7cf842d53fc305560d83f7d6
sandesh <navaltajohan@gmail.com>
2022-05-27 15:28:08 +0545

    updating

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,22 @@
+use clap::{Arg, Command};
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("echor")
+        .version("0.1")
+        .author("sandesh<justpie8@gmail.com>")
+        .about("echo command using rust")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::new("omit newline")
+                .help("Donot print new line ")
+                .takes_value(false)
+                .short('n'),
+        )
+        .get_matches();
+    println!("{:?}", matches);
 }
