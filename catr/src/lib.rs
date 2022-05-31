9c4e0cbc0201560bf26197bc6ce146ea50b05bfb
sandesh <navaltajohan@gmail.com>
2022-05-31 13:18:05 +0545

    commit

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,8 +1,27 @@
 use std::error::Error;
+type MyResult<T> = Result<T, Box<dyn Error>>;
+use clap::{Arg,Command};
+//struct for holding arguments
+//
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
 
-type MyResult<T>=Result<T,Box<dyn Error>>;
+fn get_args()->MyResult<Config>{
 
-pub fn run()->MyResult<()>{
+    let matches=Command::new("catr")
+        .version("0.1")
+        ,about("catr command using rust")
+        .arg(
+            Arg::new("files")
+
+        )
+}
+
+pub fn run() -> MyResult<()> {
     println!("Hello world");
     Ok(())
 }
