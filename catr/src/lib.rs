d4ba5a3d0c121098eec2dd8ec4ea1a973514c05c
sandesh <navaltajohan@gmail.com>
2022-06-02 13:06:44 +0545

    commit

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,15 +1,19 @@
 use std::error::Error;
 type MyResult<T> = Result<T, Box<dyn Error>>;
 use clap::{Arg, Command};
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 //struct for holding arguments
 //
 #[derive(Debug)]
+//structure of arguments
 pub struct Config {
     files: Vec<String>,
     number_lines: bool,
     number_nonblank_lines: bool,
 }
 
+//using clap for getting args required for running programs
 pub fn get_args() -> MyResult<Config> {
     let matches = Command::new("catr")
         .version("0.1")
@@ -46,6 +50,13 @@ pub fn get_args() -> MyResult<Config> {
         number_lines: line_num,
         number_nonblank_lines: line_num_nonblank,
     })
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
 
 pub fn run(config: Config) -> MyResult<()> {
